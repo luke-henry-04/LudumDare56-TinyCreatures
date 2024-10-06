1bc908e44dad1dc08e170ebf01ad0c978c9fd28b
Luke <lukehenry04@gmail.com>
2024-10-05 20:57:04 -0700

    Cabinet Setup
    Made the model and animated the top drawer on click

---
+++ LudumDare56-TinyCreatures/Assets/Drawer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+
+    public Camera camera;
+    public Animator animator;
+    public AnimationClip[] OpenCloseAnims;
+    private bool open = false;
+  
+    private void OnMouseDown()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)){
+            open = !open;
+            animator.SetBool("Open", open);
+            animator.updateMode = AnimatorUpdateMode.AnimatePhysics;
+            
+        }
+    }
+}
